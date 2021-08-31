aa584ea8c03964df68d71e2e8c8925415696cf3c
Davide Albanese <davide.albanese@gmail.com>
2021-08-31 16:56:47 +0200

    Improved merge_eggnog_mapper; minof changes

--- bin/merge_eggnog_mapper.py
+++ bin/merge_eggnog_mapper.py
@@ -14,28 +14,37 @@ parser.add_argument('-s', '--suffix', dest='suffix', required=False,
 args = parser.parse_args()
 
 
-def count(input_df, input_id, col):
+def split_annot(annot, delim=','):
+    if delim == '':
+        return list(annot)
+    else:
+        return annot.split(delim)
+
+def count(input_df, input_id, col, delim=','):
     output_df = input_df[["#query", col]].copy()
-    output_df[col] = output_df.apply(lambda row: row[col].split(','), axis=1)
+    output_df[col] = output_df.apply(lambda row: split_annot(row[col], delim),
+        axis=1)
     output_df = output_df.explode(col).reset_index(drop=True)
-    output_df = output_df.groupby([col])[["#query"]].nunique()
+    output_df = output_df.groupby([col]).nunique()
+    
     output_df = output_df.rename(columns = {"#query": input_id})
     return output_df
 
 
 cols = {
-    'GOs': 'eggnog_GOs.tsv',
-    'EC':  'eggnog_EC.tsv',
-    'KEGG_ko': 'eggnog_KEGG_ko.tsv',
-    'KEGG_Pathway': 'eggnog_KEGG_Pathway.tsv',
-    'KEGG_Module': 'eggnog_KEGG_Module.tsv',
-    'KEGG_Reaction': 'eggnog_KEGG_Reaction.tsv',
-    'KEGG_rclass': 'eggnog_KEGG_rclass.tsv',
-    'BRITE': 'eggnog_BRITE.tsv',
-    'KEGG_TC': 'eggnog_KEGG_TC.tsv',
-    'CAZy': 'eggnog_CAZy.tsv',
-    'BiGG_Reaction': 'eggnog_BiGG_Reaction.tsv',
-    'PFAMs': 'eggnog_PFAMs.tsv'
+    'COG_category': ['', 'eggnog_COG_category.tsv'],
+    'GOs': [',', 'eggnog_GOs.tsv'],
+    'EC': [',', 'eggnog_EC.tsv'],
+    'KEGG_ko': [',', 'eggnog_KEGG_ko.tsv'],
+    'KEGG_Pathway': [',', 'eggnog_KEGG_Pathway.tsv'],
+    'KEGG_Module': [',', 'eggnog_KEGG_Module.tsv'],
+    'KEGG_Reaction': [',', 'eggnog_KEGG_Reaction.tsv'],
+    'KEGG_rclass': [',', 'eggnog_KEGG_rclass.tsv'],
+    'BRITE': [',', 'eggnog_BRITE.tsv'],
+    'KEGG_TC': [',', 'eggnog_KEGG_TC.tsv'],
+    'CAZy': [',', 'eggnog_CAZy.tsv'],
+    'BiGG_Reaction': [',', 'eggnog_BiGG_Reaction.tsv'],
+    'PFAMs': [',', 'eggnog_PFAMs.tsv']
 }
 
 seed_ortholog_df = pd.DataFrame()
@@ -49,20 +58,11 @@ for input_fn in args.input:
     input_df = pd.read_table(input_fn, sep='\t', skiprows=4, skipfooter=3,
         engine='python')
     
-    tmp_df = input_df.groupby(["seed_ortholog", "eggNOG_OGs", "max_annot_lvl",
-        "COG_category", "Description", "Preferred_name"])[["#query"]].nunique()
-    tmp_df = tmp_df.rename(columns = {"#query": input_id})
-    seed_ortholog_df = pd.concat([seed_ortholog_df, tmp_df], axis=1,
-        join='outer')
-
-    for col in cols:
-        tmp_df = count(input_df, input_id, col)
+    for col, col_feats in cols.items():
+        tmp_df = count(input_df, input_id, col, delim=col_feats[0])
         count_dfs[col] = pd.concat([count_dfs[col], tmp_df], axis=1,
             join='outer')
 
-seed_ortholog_df.fillna(0, inplace=True)
-seed_ortholog_df.to_csv('eggnog_seed.tsv', sep='\t', float_format='%.d')
-
-for col in cols:
+for col, col_feats in cols.items():
     count_dfs[col].fillna(0, inplace=True)
-    count_dfs[col].to_csv(cols[col], sep='\t', float_format='%.d')
+    count_dfs[col].to_csv(col_feats[1], sep='\t', float_format='%.d')
